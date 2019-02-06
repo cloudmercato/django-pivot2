2ee0f5f5ef30d03f735f2b5a2a205b08d7eb8f63
Anthony Monthe <anthony.monthe@gmail.com>
2019-02-06 17:18:29 +0000

    Bump

--- django_pivot/__init__.py
+++ django_pivot/__init__.py
@@ -1,5 +1,5 @@
 """Easily browse your Django models"""
-VERSION = (0, 2, 5)
+VERSION = (0, 2, 6)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Anthony Monthe (2uluPro)'
 __email__ = 'amonthe@cloudspectator.com'
